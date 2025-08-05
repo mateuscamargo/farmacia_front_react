ef92694d59ce15f798ddbeaf1374ace08d001718
mateuscamargo <mateuscamargolima@gmail.com>
2025-08-05 20:35:40 -0300

    Add a Home | Navbar | Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmácia
+
+                    <div className='flex gap-4'>
+                        Produtos
+                        Categorias
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
